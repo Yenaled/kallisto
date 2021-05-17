8245f059be850587806b9f97a4d55b30ccfce918
Yenaled <delaneyk.sullivan@gmail.com>
2021-05-16 22:46:07 -0700

    revert

--- src/ProcessReads.cpp
+++ src/ProcessReads.cpp
@@ -383,7 +383,7 @@ void MasterProcessor::processReads() {
     std::vector<std::thread> workers;
     std::cout << "TODO: BEGIN" << std::endl;
     //rpV2.setup(index,opt,tc,*this);
-    //ReadProcessorV2 rpV2(index,opt,tc,*this); // PASS THIS  IN AS ARGUMENT TO BUSPROCESSOR OR see "TODO: can we really not put storage in masterprocessor?"
+    ReadProcessorV2 rpV2(index,opt,tc,*this); // PASS THIS  IN AS ARGUMENT TO BUSPROCESSOR OR see "TODO: can we really not put storage in masterprocessor?"
     //workers.emplace_back(std::thread(&rpV2)); // TODO: can we really not put storage in masterprocessor? might need to...
     /// workers.emplace_back(ReadProcessorV2(index,opt,tc,*this)); // THIS WORKS but need to figure out storage in MP...
     //rpV2 = new ReadProcessorV2(index,opt,tc,*this);
@@ -1898,6 +1898,7 @@ void ReadProcessorV2::operator()() { // TODO: seqs stack vs. heap; maybe use Rea
     std::vector<std::string> umis;
     // No reader lock since this should only be one thread
     if (mp.SR->empty()) {
+      std::cout << "TODO: DONE READING" << std::endl;
       finishedReading = true;
       condReadyToPop.notify_all();
       return;
