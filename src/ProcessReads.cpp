220a66ec8e3a0febc5d5df6e5b1f69f767260ed5
Yenaled <delaneyk.sullivan@gmail.com>
2021-05-17 18:00:51 -0700

    another

--- src/ProcessReads.cpp
+++ src/ProcessReads.cpp
@@ -1935,6 +1935,7 @@ void ReadProcessorV2::operator()() { // TODO: seqs stack vs. heap; maybe use Rea
           condReadyToPush.wait(lock);
         }
         readStorage.push(sData);
+        std::cout << "zz" << sData.seqs.size() << " " << sData.seqs[0].first << sData.seqs[0].second << " " << sData.seqs[1].first << sData.seqs[1].second << std::endl; // 139810 printed 906 times = good
         // TODO: std::cout mp.opt.threads and a sequence (to make sure mp reference transferred through...)
         //std::cout << readbatch_id << ":" << mp.opt.threads << std::endl;
       }
@@ -1964,7 +1965,9 @@ bool ReadProcessorV2::fetchSequences(std::vector<std::pair<const char*, int>>& s
     }
   }
   SequenceData sData = std::move(readStorage.front());
+  std::cout << "yy" << sData.seqs.size() << " " << sData.seqs[0].first << sData.seqs[0].second << " " << sData.seqs[1].first << sData.seqs[1].second << std::endl; // 139810 printed 906 times = good
   readStorage.pop();
+  std::cout << "xx" << sData.seqs.size() << " " << sData.seqs[0].first << sData.seqs[0].second << " " << sData.seqs[1].first << sData.seqs[1].second << std::endl; // 139810 printed 906 times = good
   // TODO: MAX CAPACITY FOR QUEUE
   // TODO: see if RequestShutdown() and finishedReading needed -- e.g. if threads are waiting on V2::fetchSequences but there's nothing left to read (operator returns; but is that enough?)
   // // // yeah, might need to finishedReading=true up there in the if mp sr empty -- but then again, a thread-safe empty() function might be better
