d242c0c71dd98cb810fbe3a15e5006f459e8dba8
Yenaled <delaneyk.sullivan@gmail.com>
2021-05-18 16:59:07 -0700

    try 8

--- src/ProcessReads.cpp
+++ src/ProcessReads.cpp
@@ -1914,7 +1914,7 @@ ReadProcessorV2::~ReadProcessorV2() {
 }
 
 void ReadProcessorV2::operator()() { // TODO: seqs stack vs. heap; maybe use ReadProcessorV2 as [stack] storage itself!!! <- yes!
-  int storage_limit = mp.opt.threads*2 + 1; // TODO: mp.opt.threads+1
+  int storage_limit = mp.opt.threads*8 + 1; // TODO: mp.opt.threads+1
   //std::chrono::duration<double, std::milli> cc;
   //std::chrono::steady_clock::time_point begin11 = std::chrono::steady_clock::now();
   
